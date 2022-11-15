de44c4a8e8102658cd52354b3ee6cb71de4f9c83
Eliot Stock <1@eliotstock.com>
2022-11-15 15:41:15 +1300

    Branch change

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world 15:27",
+            "message": "hello world 15:41 in branch",
             # "location": ip.text.replace("\n", "")
         }),
     }
